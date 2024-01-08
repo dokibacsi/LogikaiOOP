e4b18eecd592dc1ff4e0c0003b4b579dd734c35e
dokibacsi <kvalcsi02@gmail.com>
2024-01-08 09:45:58 +0100

    allitasok_elso_oop

--- src/logikaifeladat/Logikaifeladat.java
+++ src/logikaifeladat/Logikaifeladat.java
@@ -1,21 +1,12 @@
-
 package logikaifeladat;
-
-import java.util.Scanner;
 
 public class Logikaifeladat {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Adjon meg egy szöveget!: ");
-        String beker = sc.nextLine();
-        System.out.println("Sc:"+beker);
-        System.out.println("");
-        System.out.println(""); 
-        System.out.println(""); 
-        System.out.println(""); 
-        System.out.println(""); 
-        System.out.println(""); 
+        new Allitasok(new String[]{
+            "1. Állítás",
+            "2. Állítás",
+            "3. Állítás"
+        });
     }
-    
 }
