928d16bf322ff63a3fff9fcf226e6fb2b86fd08c
ASDleves <ricsi20020302@gmail.com>
2024-01-08 09:23:56 +0100

    scanner hozzáad

--- src/logikaifeladat/Logikaifeladat.java
+++ src/logikaifeladat/Logikaifeladat.java
@@ -1,9 +1,15 @@
 
 package logikaifeladat;
 
+import java.util.Scanner;
+
 public class Logikaifeladat {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adjon meg egy szöveget!: ");
+        String beker = sc.nextLine();
+        System.out.println("Sc:"+beker);
         System.out.println("");
         System.out.println(""); 
         System.out.println(""); 
