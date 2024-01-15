df2b2acbef0643505872a7cd2dbe341d99ce8f94
ASDleves <ricsi20020302@gmail.com>
2024-01-15 11:04:08 +0100

    folosleges fajlok torlese

--- src/model/LadaKezeles.java
+++ src/model/LadaKezeles.java
@@ -11,12 +11,13 @@ public class LadaKezeles {
         initializeLadak();
     }
 
-    private void initializeLadak() {
+    private Lada[] initializeLadak() {
         // Initialize your Lada objects here
         ladak = new Lada[3];
         ladak[0] = new Lada("Én rejtem a kincset!", "Arany", false, 1);
         ladak[1] = new Lada("Nem én rejtem!", "Ezüst", true, 2);
         ladak[2] = new Lada("az arany hazudik!", "Bronz", false, 3);
+        return ladak; 
     }
 
     public void startGame() {
